0837e42805b3f89c6f37c713c8a92f3f6a5d1357
Alvin Ng <alvinnws4546@gmail.com>
2024-05-19 20:15:23 +0800

    Tictactoe on tutorial

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  standalone: true,
+  imports: [],
+  template: `
+    <button> {{  value  }}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+})
+export class SquareComponent {
+
+  @Input()
+  value: 'X' | 'O' | undefined;
+  
+}
