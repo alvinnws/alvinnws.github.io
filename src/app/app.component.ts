7a4979a407602532c34234650a85ea710bf2dc0f
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-05-20 06:34:27 +0000

    Testing Navbar and --deploy-url use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { BoardComponent } from "./board/board.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, BoardComponent]
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, BoardComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'portfolio';
