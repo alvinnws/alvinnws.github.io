a8c570e4985fb2da9f7f9df4e7cebbe894926fc6
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-05-21 03:00:11 +0000

    Added home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { BoardComponent } from './tictactoe/board/board.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: 'minigames/tictactoe', component: BoardComponent }
+    { path: 'minigames/tictactoe', component: BoardComponent },
+    { path: '', component: HomeComponent }
 ];
