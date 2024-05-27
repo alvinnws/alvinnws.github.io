3eaa9e57035f873ebbbe6901d5340e832b4f972b
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-05-27 06:45:30 +0000

    Added mobile support for nav, fixed tictactoe on iOS

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule, MatMenuModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
--- src/app/tictactoe/square/square.component.ts
+++ src/app/tictactoe/square/square.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
   template: `
     <button> {{  value  }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; border-radius: 0;}']
+  styles: ['button { width: 100%; height: 100%; display:flex; justify-content:center; align-items: center; font-size: 50px !important; border-radius: 0; -webkit-appearance: none;}']
 })
 export class SquareComponent {
 
