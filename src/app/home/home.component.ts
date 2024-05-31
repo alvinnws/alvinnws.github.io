34e9698b311a5b432f9f4b22ebf58bf716340885
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-05-31 09:12:10 +0000

    Updated home page a bit, added 404 page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule, MatMenuModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
