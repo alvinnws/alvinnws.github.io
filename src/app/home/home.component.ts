b61d99c0fa17fc65954491d5c61f4805bfa015aa
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-06-03 04:02:23 +0000

    Revert "Updated home page a bit, added 404 page"
    This reverts commit 34e9698b311a5b432f9f4b22ebf58bf716340885.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule, MatMenuModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
