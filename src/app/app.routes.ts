c1cd8d33a02924c7b8caf689065e53e8906c6a8b
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-06-04 08:57:14 +0000

    Added contact page, re-routed tictactoe to ttic-tac-toe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { BoardComponent } from './tictactoe/board/board.component';
 import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
-    { path: 'minigames/tictactoe', component: BoardComponent },
+    { path: 'minigames/tic-tac-toe', component: BoardComponent },
     { path: '', component: HomeComponent },
+    { path: 'contact-me', component: ContactComponent },
     { path: '**', pathMatch:'full', component: PagenotfoundComponent }
 ];
