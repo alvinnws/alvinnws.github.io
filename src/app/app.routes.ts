90ec2e2b0dde4a9b07491731879d060aede01420
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-06-12 03:21:41 +0000

    Adjusted app routes/component for links page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ContactComponent } from './contact/contact.component';
 import { MinigamesComponent } from './minigames/minigames.component';
+import { LinksComponent } from './links/links.component';
 
 export const routes: Routes = [
     { path: '', title: 'Alvin Ng Wei Sing | Home', component: HomeComponent },
+    { path: 'links', component: LinksComponent},
     { path: 'contact-me', title: 'Contact Me | Alvin Ng', component: ContactComponent },
     { path: 'minigames', title: 'Minigames | Alvin Ng',
         children: [
             { path: 'tic-tac-toe', title: 'Tic-Tac-Toe | Alvin Ng', component: BoardComponent },
             { path: '', pathMatch: 'full', component: MinigamesComponent }
         ]},
-    { path: '**', pathMatch:'full', component: PagenotfoundComponent }
+    { path: '**', pathMatch:'full', component: PagenotfoundComponent },
 ];
