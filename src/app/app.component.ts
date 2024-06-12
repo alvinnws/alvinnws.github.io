bf5bd4c9520f208e821c8b7bc5de8262fdca067e
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-06-12 03:21:41 +0000

    Adjusted app routes/component for links page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { BoardComponent } from "./minigames/tictactoe/board/board.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-root',
@@ -11,5 +12,7 @@ import { NavbarComponent } from "./navbar/navbar.component";
     imports: [RouterOutlet, RouterLink, RouterLinkActive, BoardComponent, NavbarComponent]
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'portfolio'; 
+  constructor(public location: Location) {
+  }
 }
