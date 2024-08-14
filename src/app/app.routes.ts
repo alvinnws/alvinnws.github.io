21935cc67a010b8993fec7b356e5603d91d33304
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-08-14 09:15:11 +0000

    Beginning blog section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ContactComponent } from './contact/contact.component';
 import { MinigamesComponent } from './minigames/minigames.component';
 import { LinksComponent } from './links/links.component';
+import { BlogComponent } from './blog/blog.component';
+import { TemplateComponent } from './blog/blogtemplate/blogtemplate.component';
 
 export const routes: Routes = [
     { path: '', title: 'Alvin Ng Wei Sing | Home', component: HomeComponent },
@@ -15,5 +17,11 @@ export const routes: Routes = [
             { path: 'tic-tac-toe', title: 'Tic-Tac-Toe | Alvin Ng', component: BoardComponent },
             { path: '', pathMatch: 'full', component: MinigamesComponent }
         ]},
+    { path: 'blog',
+        children: [
+            { path: 'template', title: 'Blog Template | Alvin Ng', component: TemplateComponent },
+            { path: '', title: 'Blog | Alvin Ng', component: BlogComponent },
+        ]
+    },
     { path: '**', pathMatch:'full', component: PagenotfoundComponent },
 ];
