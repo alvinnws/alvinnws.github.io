e8d6cbec550a5d15ad62367ad32f8b2bf893be5f
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-08-14 09:15:11 +0000

    Beginning blog section

---
+++ src/app/blog/blogtemplate/blogtemplate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './blogtemplate.component.html',
+  styleUrl: './blogtemplate.component.scss'
+})
+export class TemplateComponent {
+
+}
