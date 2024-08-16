5bcb117e8e4c89eefe2ad02b208599310e12176a
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2024-08-16 08:26:29 +0000

    Updating URLs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,10 @@ import { MinigamesComponent } from './minigames/minigames.component';
 import { LinksComponent } from './links/links.component';
 import { BlogComponent } from './blog/blog.component';
 import { TemplateComponent } from './blog/blogtemplate/blogtemplate.component';
+import { WorkComponent } from './work/work.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EducationComponent } from './education/education.component';
+import { CertificatesComponent } from './certificates/certificates.component';
 
 export const routes: Routes = [
     { path: '', title: 'Alvin Ng Wei Sing | Home', component: HomeComponent },
@@ -21,6 +25,26 @@ export const routes: Routes = [
         children: [
             { path: 'template', title: 'Blog Template | Alvin Ng', component: TemplateComponent },
             { path: '', title: 'Blog | Alvin Ng', component: BlogComponent },
+        ]
+    },
+    { path: 'work-experience',
+        children: [
+            { path: '', title: 'Career | Alvin Ng', component: WorkComponent }
+        ]
+    },
+    { path: 'projects',
+        children: [
+            { path: '', title: 'Projects | Alvin Ng', component: ProjectsComponent }
+        ]
+    },
+    { path: 'education',
+        children: [
+            { path: '', title: 'Education | Alvin Ng', component: EducationComponent }
+        ]
+    },
+    { path: 'certificates',
+        children: [
+            { path: '', title: 'Certificates | Alvin Ng', component: CertificatesComponent}
         ]
     },
     { path: '**', pathMatch:'full', component: PagenotfoundComponent },
