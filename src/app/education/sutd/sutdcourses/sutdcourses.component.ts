f219c2acd64b96bacca337326debc5ac6b97e065
Alvin Ng <alvinnws4546@gmail.com>
2024-10-05 16:04:22 +0800

    Reducing bloat

--- src/app/education/sutd/sutdcourses/sutdcourses.component.ts
+++ src/app/education/sutd/sutdcourses/sutdcourses.component.ts
@@ -4,8 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-sutdcourses',
   standalone: true,
   imports: [],
-  templateUrl: './sutdcourses.component.html',
-  styleUrl: './sutdcourses.component.scss'
+  template: `
+    <h1>SUTD Coursework</h1>
+    <h2>Term 1 (Sep-Dec 2024)</h2>
+    <p>01.010 Freshmore Communication Programme</p>
+    <p>01.011 Professional Practice Programme</p>
+    <p>01.012 UPOP Essential Module</p>
+    <p>02.003 Social Science: Understanding Behaviour, Culture & Society</p>
+    <p>10.014 Computational Thinking for Design</p>
+    <p>10.015 Physical World</p>
+
+    <h2>Compact (Mar 2024)</h2>
+    <p>10.013 Modelling & Analysis | Grade: Pass</p>
+  `,
+  styles: ``
 })
 export class SUTDCoursesComponent {
 
