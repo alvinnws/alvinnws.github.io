8850637ef429da169a095250c9e68bbddd363ca7
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2026-05-14 17:04:22 +0800

    feat: Blog subpage auto lists posts on build

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,29 @@
+import pluginFilters from "./_config/filters.js";
+
 export default async function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy("src/styles.css");
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/CNAME");
     
+    eleventyConfig.addPlugin(pluginFilters);
+
+    eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
+		if (data.draft) {
+			data.title = `${data.title} (draft)`;
+		}
+
+		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+			return false;
+		}
+	});
 };
 
 export const config = {
     dir: {
         input: "src",
         includes: "../_includes",
-        data: "../data",
+        data: "../_data",
         output: "docs"
     }
 };
